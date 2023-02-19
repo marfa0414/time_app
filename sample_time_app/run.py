c57296c2ffbe72dbc815eaa08da03a502bb51f70
marfa0414 <1021785867@qq.com>
2023-02-19 15:37:07 -0500

    first commit

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,11 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+import datetime
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
 
+@app.route('/time')
+def get_time():
+    datetime_p = datetime.datetime.now()
+    datetime_s = datetime.datetime.strftime(datetime_p, '%Y-%m-%d %H:%M:%S')
+    return datetime_s
 
 app.run(host='0.0.0.0',
         port=8080,
